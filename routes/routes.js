5de395f9a6988d9bccf655ec353186c5ad893613
Chick <cheecken0@hotmail.com>
2018-08-17 07:25:15 +0800

    updated logging of api
    debugging routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,11 @@ const routes = require('express').Router();
 const apiRoutes = require('express').Router();
 const journal = require('./journalRoute');
 
+
 routes.use('/api', apiRoutes);
+apiRoutes.use('/', (req, res)=>{
+    res.status(200).json({ status: "OK"});
+})
 
 apiRoutes.use('/journal', journal);
 
--- server.js
+++ server.js
@@ -7,21 +7,9 @@ const config = require("./config/config");
 const connection = require('express-myconnection');
 const mysql = require("mysql");
 const routes = require('./routes/routes');
-const Docs = require('express-api-doc');
 
 let app = express();
 app.server = http.createServer(app);
-
-// express api documentation
-const dock = new Docs(app);
-dock.track({
-    path: './docs/examples.txt' // responses and requests will save here
-})
-
-dock.generate({
-    path: './docs/template.html',
-    examples: './docs/examples.txt'
-});
 
 // logger
 app.use(morgan('dev'));
@@ -37,10 +25,10 @@ app.use(bodyParser.json({
 
 app.use(connection(mysql, config.database));
 
-app.use('*', routes)
+app.use('*', routes);
 
 app.server.listen(process.env.PORT || config.port, () => {
     console.log(`Started on port ${app.server.address().port}`);
-})
+});
 
 module.exports = app;
